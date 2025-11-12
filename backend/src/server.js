5009224677823c119f6f9e10e980f888a3ae742b
p4n1k0 <gabre.tarick@gmail.com>
2025-11-12 13:27:42 -0300

    POST e GET users

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use('/api', userRoutes);
 
 // Conecta ao banco
 connectDB();
