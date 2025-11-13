ee0f9016b2cf8b978e1e3dada5adead4be8c7929
p4n1k0 <gabre.tarick@gmail.com>
2025-11-13 13:46:53 -0300

    authMiddleware

--- backend/src/routes/announcementRoutes.js
+++ backend/src/routes/announcementRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { createAnnouncement, listAnnouncements } from "../controllers/announcementController.js";
+import { verifyToken } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", createAnnouncement);
+router.post("/", verifyToken, createAnnouncement);
 router.get("/", listAnnouncements);
 
 export default router;
